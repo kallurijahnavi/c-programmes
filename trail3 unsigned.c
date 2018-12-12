1a752b1c5fce0308094073cb31689f50f3e088a4
Kalluri Jahnavi <45787321+kallurijahnavi@users.noreply.github.com>
2018-12-12 15:38:23 +0530

    Create trail3 unsigned.c

---
+++ trail3 unsigned.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+  unsigned i=-1;
+  int j=-4;
+  printf("%u\n",i+j);
+  printf("%d",i+j);
+}
+
+
+o\p:
+4294967291                                                                                                                                           
+-5 
