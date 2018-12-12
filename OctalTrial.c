71f905efadefc1204d24ed3b1b76492e7fe4916a
Kalluri Jahnavi <45787321+kallurijahnavi@users.noreply.github.com>
2018-12-12 15:49:11 +0530

    Create OctalTrial.c

---
+++ OctalTrial.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+    int var=052;
+    printf("%d\n",var);
+    printf("%o",var);
+}
+
+o\p:
+42                                                                                                                                                   
+52
